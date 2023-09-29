a730baabbda0871cd94a8b9fca682255013cfd5f
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2023-09-29 18:12:35 +0200

    config

---
+++ spec/app.spec.ts
@@ -0,0 +1,13 @@
+import {Component} from "../src/components/component";
+
+let app: Component;
+
+describe('A suite is just a function', function () {
+	beforeEach(() => {
+
+	});
+
+	it('and so is a spec', function () {
+
+	});
+});
