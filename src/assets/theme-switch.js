5c23375322c118bd42dce4362924cb80370a49f7
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2023-10-09 20:25:40 +0200

    valheim-food

--- src/assets/theme-switch.js
+++ src/assets/theme-switch.js
@@ -2,7 +2,19 @@ $(".darkmode").click(function () {
 	$("body").toggleClass("dark");
 	if ($("body").hasClass("dark")) {
 		document.getElementById("theme").href = "assets/darkly.bootstrap.min.css";
+		$(".no-thead-dark")
+			.removeClass("no-thead-dark")
+			.addClass("thead-dark");
+		$(".no-table-dark")
+			.removeClass("no-table-dark")
+			.addClass("table-dark");
 	} else {
 		document.getElementById("theme").href = "assets/original.bootstrap.min.css";
+		$(".thead-dark")
+			.removeClass("thead-dark")
+			.addClass("no-thead-dark");
+		$(".table-dark")
+			.removeClass("table-dark")
+			.addClass("no-table-dark");
 	}
 });
