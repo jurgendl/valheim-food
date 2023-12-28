254798a5d420a61a7941e406b0f35f46f7ec0cd1
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2023-12-28 02:04:23 +0100

    theme-switch

--- src/assets/theme-switch.js
+++ src/assets/theme-switch.js
@@ -1,20 +1,33 @@
+function darkModeToggle(isDarkMode) {
+    document.getElementById("theme").href = isDarkMode ? "assets/darkly.bootstrap.min.css" : "assets/original.bootstrap.min.css";
+    if (isDarkMode) {
+        $(".no-thead-dark")
+            .removeClass("no-thead-dark")
+            .addClass("thead-dark");
+        $(".no-table-dark")
+            .removeClass("no-table-dark")
+            .addClass("table-dark");
+    } else {
+        $(".thead-dark")
+            .removeClass("thead-dark")
+            .addClass("no-thead-dark");
+        $(".table-dark")
+            .removeClass("table-dark")
+            .addClass("no-table-dark");
+    }
+}
+
 $(".darkmode").click(function () {
-	$("body").toggleClass("dark");
-	if ($("body").hasClass("dark")) {
-		document.getElementById("theme").href = "assets/darkly.bootstrap.min.css";
-		$(".no-thead-dark")
-			.removeClass("no-thead-dark")
-			.addClass("thead-dark");
-		$(".no-table-dark")
-			.removeClass("no-table-dark")
-			.addClass("table-dark");
-	} else {
-		document.getElementById("theme").href = "assets/original.bootstrap.min.css";
-		$(".thead-dark")
-			.removeClass("thead-dark")
-			.addClass("no-thead-dark");
-		$(".table-dark")
-			.removeClass("table-dark")
-			.addClass("no-table-dark");
-	}
+    $("body").toggleClass("dark");
+    const isDarkMode = $("body").hasClass("dark");
+    darkModeToggle(isDarkMode);
+    localStorage.setItem("dark-mode", isDarkMode);
+});
+
+// on document load, check if dark mode is set and apply it
+addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+    const isDarkMode = localStorage.getItem("dark-mode") === "true";
+    if (isDarkMode) $("body").addClass("dark");
+    darkModeToggle(isDarkMode);
 });
