10c5d9a731a4e64d3ea8a615dcffdb86b287857f
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2024-02-08 13:46:12 +0100

    build config

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,16 +3,17 @@ const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	mode: 'development',
-	devtool: 'source-map',
-	devServer: {
-		port: 7584,
-		open: {
-			target: ['valheim-food.html'],
-			app: {
-				name: 'chrome',	// The browser application name is platform-dependent. Don't hard code it in reusable modules. For example, 'Chrome' is 'Google Chrome' on macOS, 'google-chrome' on Linux, and 'chrome' on Windows.
-				//arguments: ['--incognito', '--new-window'],
-			},
-		},
-	},
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        port: 7584,
+        open: {
+            target: ['valheim-food.html'],
+            app: {
+                name: 'chrome',	// The browser application name is platform-dependent. Don't hard code it in reusable modules. For example, 'Chrome' is 'Google Chrome' on macOS, 'google-chrome' on Linux, and 'chrome' on Windows.
+                arguments: ['--new-window'],
+                //arguments: ['--incognito', '--new-window'],
+            },
+        },
+    },
 });
